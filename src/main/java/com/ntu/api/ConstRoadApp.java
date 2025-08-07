0c894467665f18fc980c31eee4373a39f99ed4df
DarkPoul <poulpotter1@gmail.com>
2025-08-07 11:12:32 +0300

    initial

--- src/main/java/com/ntu/api/ConstRoadApp.java
+++ src/main/java/com/ntu/api/ConstRoadApp.java
@@ -8,13 +8,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class ConstRoadApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Constant Road 1.2.5");
         primaryStage.setResizable(false);
-        Parent main = FXMLLoader.load(getClass().getResource("/com/ntu/api/fx/model/main.fxml"));
+        Parent main = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/ntu/api/fx/model/main.fxml")));
         Main.start();
         primaryStage.setScene(new Scene(main));
         primaryStage.show();
